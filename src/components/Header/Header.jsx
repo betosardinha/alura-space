443bd91694a411ea97ae3fb9f8c0c1e0a244cf9a
Beto Sardinha <sardinhabeto55@gmail.com>
2023-06-04 19:29:29 -0300

    refactor(Header): update logo image name

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Logo from '../../images/logo-type-white.svg';
+import LogoImg from '../../images/logo-type-white.svg';
 import SearchIcon from '../../images/icons/search.svg';
 
 import styles from './Header.module.scss';
@@ -7,7 +7,7 @@ import styles from './Header.module.scss';
 function Header() {
   return (
     <header className={styles.header}>
-      <img src={Logo} alt="Logo do Alura Space" />
+      <img src={LogoImg} alt="Logo do Alura Space" />
       <div className={styles.header__container}>
         <input className={styles.header__input} type="text" placeholder="O que você procura?" />
         <img src={SearchIcon} alt="Ícone de lupa" />
