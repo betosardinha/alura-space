27d90d98dd7b3b7f655877e94358120ceee50491
Beto Sardinha <sardinhabeto55@gmail.com>
2023-06-16 23:01:57 -0300

    style(Header): change logo width

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,7 +7,7 @@ import styles from './Header.module.scss';
 function Header() {
   return (
     <header className={styles.header}>
-      <img src={LogoImg} alt="Logo do Alura Space" />
+      <img className={styles.header__logo} src={LogoImg} alt="Logo do Alura Space" />
       <div className={styles.header__container}>
         <input className={styles.header__input} type="text" placeholder="O que você procura?" />
         <img src={SearchIcon} alt="Ícone de lupa" />
