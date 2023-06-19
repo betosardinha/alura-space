eb54b7d2b87bf1daba774b198757f31c240403a2
Beto Sardinha <sardinhabeto55@gmail.com>
2023-06-18 22:41:35 -0300

    chore: add absolute imports

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import Banner from '../../components/Banner';
-import Footer from '../../components/Footer';
-import Gallery from '../../components/Gallery';
-import Header from '../../components/Header';
-import Menu from '../../components/Menu';
-import Popular from '../../components/Popular';
+import Banner from 'components/Banner';
+import Footer from 'components/Footer';
+import Gallery from 'components/Gallery';
+import Header from 'components/Header';
+import Menu from 'components/Menu';
+import Popular from 'components/Popular';
 
 import styles from './Homepage.module.scss';
 
