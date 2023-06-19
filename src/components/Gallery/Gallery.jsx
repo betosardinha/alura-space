a44b86f3d5334f8cf549b14baf65eb5a91f731ac
Beto Sardinha <sardinhabeto55@gmail.com>
2023-06-18 23:54:32 -0300

    feat(Tags): add filter images

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Cards from '../Cards';
 import Tags from '../Tags';
@@ -8,11 +8,19 @@ import images from './images.json';
 import styles from './Gallery.module.scss';
 
 function Gallery() {
+  const [items, setItems] = useState(images);
+  const tags = [...new Set(images.map((value) => value.tag))];
+
+  const imagesByTag = (tag) => {
+    const filteredImages = images.filter((image) => image.tag === tag);
+    setItems(filteredImages);
+  };
+
   return (
     <section className={styles.gallery}>
       <h2>Navegue pela galeria</h2>
-      <Tags />
-      <Cards items={images} />
+      <Tags tags={tags} imagesByTag={imagesByTag} setItems={setItems} />
+      <Cards items={items} />
     </section>
   );
 }
--- src/components/Tags/Tags.jsx
+++ src/components/Tags/Tags.jsx
@@ -1,19 +1,47 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
+import images from 'components/Gallery/images.json';
 
 import styles from './Tags.module.scss';
 
-function Tags() {
+function Tags({ tags, imagesByTag, setItems }, handleKeyDown) {
   return (
     <div className={styles.tags}>
       <p>Filtre por tags:</p>
       <ul className={styles.tags__list}>
-        <li>Estrelas</li>
-        <li>Galáxia</li>
-        <li>Lua</li>
-        <li>Planetas</li>
+        <li>
+          <button
+            type="button"
+            key="Todas"
+            onClick={() => setItems(images)}
+            onKeyDown={() => handleKeyDown}
+          >
+            Todas
+          </button>
+        </li>
+
+        {tags.map((tag) => (
+          <li key={tag}>
+            <button
+              type="button"
+              key={tag}
+              onClick={() => imagesByTag(tag)}
+              onKeyDown={() => handleKeyDown}
+            >
+              {tag}
+            </button>
+          </li>
+        ))}
       </ul>
     </div>
   );
 }
+
+Tags.propTypes = {
+  tags: PropTypes.arrayOf(PropTypes.string).isRequired,
+  imagesByTag: PropTypes.func.isRequired,
+  setItems: PropTypes.func.isRequired,
+};
 
 export default Tags;
